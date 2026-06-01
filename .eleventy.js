cb7f55a3b18daa0b31dc852660ef395a72baddac
Tome da Costa Lima <tomecl@motorola.com>
2026-06-01 14:25:56 -0300

    fix: baseplugin for paths

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,19 @@
+const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
+
 module.exports = function(eleventyConfig) {
     // Tell Eleventy to copy your assets folder directly to the output (_site) folder
+    eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
     eleventyConfig.addPassthroughCopy("assets");
 
     return {
         dir: {
             input: ".",         // Look in the root folder for pages/posts
             output: "_site"     // Spit out the finished website here
-        }
+        },
+        pathPrefix: "/your-repository-name/",
+        markdownTemplateEngine: "njk",
+        htmlTemplateEngine: "njk"
     };
+
+
 };
